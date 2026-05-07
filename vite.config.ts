c0759b99f7388efb60b37eae280d6237d4bb3f82
Ashton Wiley <ashtontwiley2005@gmail.com>
2026-05-07 18:38:53 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-   base: '/media-multi-family-edition/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
